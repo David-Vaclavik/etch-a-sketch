92c2915c8871c7b850e2bc9f75e23c457c5f7d8c
Geny88 <geny1794@gmail.com>
2025-07-04 15:23:48 +0200

    Site completed with the following changes: - Updated engine.js to fix the game logic. - Modified index.html to enhance the user interface. - Adjusted style.css for better responsiveness and aesthetics. - Added Rainbow mode

--- engine.js
+++ engine.js
@@ -44,7 +44,7 @@ function newSquares() {
     attachHoverListeners();
 }
 
-//  -non grid on page load, only after click new grid btn
+//  Default grid on page load
 for (let i = 1; i <= squaresPerSide * squaresPerSide; i++) {
     const divSquare = document.createElement("div");
     divSquare.classList.add("grid-square");
@@ -52,33 +52,59 @@ for (let i = 1; i <= squaresPerSide * squaresPerSide; i++) {
 }
 attachHoverListeners();
 
-// newSquares();
+// Enables Rainbow function
+let rainbowMode = false;
+// let randomColor = (Math.floor(Math.random() * 360));
+let resetValue = 0;
 
 // adds hover effect on new grids created with a button
 function attachHoverListeners() {
     const currentGridSquares = document.querySelectorAll(".grid-square");
     currentGridSquares.forEach((square) => {
+        let randomColor = (Math.floor(Math.random() * 360));
+        let opacity = 0.1;
         square.addEventListener("mouseover", () => {
-            square.setAttribute("style", "background: grey;")
+            if (rainbowMode === false) {
+                square.setAttribute("style", `background: black; opacity: ${opacity};`);
+            } else if (rainbowMode) {
+                square.setAttribute("style", `background: hsl(${randomColor}, 100%, 50%);`);
+            }
+
+            // checks opacity value and increases opacity by 0.1
+            const computedStyle = window.getComputedStyle(square);
+            const currentOpacity = parseFloat(computedStyle.getPropertyValue("opacity"));
+            if (currentOpacity < 1) {
+                opacity += 0.1;
+            }
         });
     });
 }
 
+// function getRainbow() {
+//     console.log(Math.floor(Math.random() * 360));
+//     return Math.floor(Math.random())
+// }
+
+
 function reset() {
     const currentGridSquares = document.querySelectorAll(".grid-square");
     currentGridSquares.forEach((square) => 
-        square.setAttribute("style", "background: white;"));
+        square.setAttribute("style", "background: white; opacity: 1"));
+    resetValue = 1;
+    // This resets opacity
+    attachHoverListeners();
 }
 
-// Buttons
+// Buttons selector
 const btnNewGrid = document.querySelector(".new-grid");
 const btnReset = document.querySelector(".reset");
-
+const btnRainbow = document.querySelector(".rainbow");
 
 // Button callback
 function onUserChoice(callback) {
     btnNewGrid.addEventListener("click", () => callback("NewGrid"));
     btnReset.addEventListener("click", () => callback("Reset"));
+    btnRainbow.addEventListener("click", () => callback("Rainbow"));
 }
 
 // Calling for callback
@@ -87,6 +113,14 @@ onUserChoice((choice) => {
         reset();
     } else if (choice === "NewGrid") {
         newSquares();
+    } else if (choice === "Rainbow") {
+        rainbowMode = !rainbowMode;
+        if (rainbowMode) {
+            btnRainbow.classList.add("active");
+        } else if (rainbowMode === false) {
+            btnRainbow.classList.remove("active");
+        }
+        // getRainbow();
     }
 });
 
